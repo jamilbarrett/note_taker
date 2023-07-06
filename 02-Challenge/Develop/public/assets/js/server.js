d05e3e7bb7504aa3a0caf6560742bf2b04526883
Jamil Barrett <jamilbarrett@jamils-imac.myfiosgateway.com>
2023-07-05 22:22:32 -0400

    added post

--- 02-Challenge/Develop/public/assets/js/server.js
+++ 02-Challenge/Develop/public/assets/js/server.js
@@ -2,6 +2,8 @@ const fs = require('fs')
 const express = require('express')
 const app = express()
 const {v4} = require('uuid')
+const PORT = 3001
+const path = require('path')
 
 app.use(express.static('public'))
 
@@ -22,4 +24,24 @@ app.get('/api/notes', (req, res) => {
     })
 })
 
-app.post()
+app.post('/api/notes', (req, res) => {
+    const filePath = path.join(__dirname, '/db.json')
+
+    const notes = JSON.parse(fs.readFileSync(filePath, 'utf8'))
+  
+    const newNote = {
+      id: uuidv4(),
+      ...req.body
+    };
+  
+
+    notes.push(newNote)
+  
+    fs.writeFile(filePath, JSON.stringify(notes, null, 2))
+  
+    // Send the new note as the response
+    res.status(201).json(newNote)
+  });
+
+app.listen(PORT, () =>
+console.log('Server started at  http://localhost:${PORT}'))
