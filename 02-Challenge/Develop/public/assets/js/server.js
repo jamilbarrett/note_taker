1c9fc8e2d41246e5507e6e2783b556b43309c34d
Jamil Barrett <jamilbarrett@jamils-imac.myfiosgateway.com>
2023-07-05 22:04:07 -0400

    added uuid

--- 02-Challenge/Develop/public/assets/js/server.js
+++ 02-Challenge/Develop/public/assets/js/server.js
@@ -1,16 +1,25 @@
 const fs = require('fs')
 const express = require('express')
 const app = express()
+const {v4} = require('uuid')
 
 app.use(express.static('public'))
 
 
 
 app.get('*', (req, res) => {
-    res.sendFile(__dirname + 'public/index.html' )
+    res.sendFile(__dirname + '/public/index.html' )
 })
 
 app.get('/notes', (req, res) => {
-    res.sendFile(__dirname + 'public/notes.html')
+    res.sendFile(__dirname + '/public/notes.html')
 })
 
+app.get('/api/notes', (req, res) => {
+    fs.readFile('db.json', 'utf-8', (err, data) => {
+        if(err) throw err
+        res.send(data)
+    })
+})
+
+app.post()
