773eea13d4fd291b652fe00740790ac2521ba582
Jamil Barrett <jamilbarrett@Jamils-iMac.local>
2023-07-11 09:42:47 -0400

    correct variable port

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const path = require('path');
 const express = require('express');
 const app = express();
 const { v4: uuidv4 } = require('uuid');
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 
 // Middleware
 app.use(express.static('./public'));
